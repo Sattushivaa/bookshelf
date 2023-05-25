9499e481d3cfa319df12c0cad952ba982299c353
Satyam Bharti <73614498+Sattushivaa@users.noreply.github.com>
2023-05-25 11:06:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 let ld = new Loader('loading bookshelf');
 ld.load();
 let notif = new Notif();
-//const server = new WebSocket('wss://teal-erratic-exoplanet.glitch.me',['mainserver']);
+const server = new WebSocket('wss://teal-erratic-exoplanet.glitch.me',['mainserver']);
 
-const server = new WebSocket('ws://localhost:8080',['mainserver']);
+//const server = new WebSocket('ws://localhost:8080',['mainserver']);
 
 server.onopen =()=>ld.discard();
 const navitem = document.getElementsByClassName('navitem');
